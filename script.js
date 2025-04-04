311639cd85a7d0c122d0f5914c18a3df9d86bcc6
Elie Ngiene <elie.ngiene@gmail.com>
2025-04-04 03:41:59 +0100

    add quiz

--- script.js
+++ script.js
@@ -138,6 +138,15 @@ document.getElementById('questionnaireForm').addEventListener('submit', function
                         }
                     ]
                 },
+                "Plus grande reussite": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": document.querySelector('textarea[name="reussite"]').value
+                            }
+                        }
+                    ]
+                },
                 "Profession": {
                     "select": {
                         "name": document.querySelector('input[name="profession"]:checked').value
